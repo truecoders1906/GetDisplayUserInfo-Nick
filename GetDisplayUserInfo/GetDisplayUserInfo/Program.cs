0c87b8368b1442005efb08161d3c69c26d91740f
Nick Vannoy <svannoy6@gmail.com>
2019-07-01 13:55:48 -0500

    for loop to stop name of pet from popping up over and over

---
+++ GetDisplayUserInfo/GetDisplayUserInfo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GetDisplayUserInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + firstName + ". " + "It's very nice to meet you!");
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine(lastName + "... " + "that is a very unique last name!");
+
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumberResponse = Console.ReadLine();
+            int favoriteNumber = int.Parse(favoriteNumberResponse);
+
+            Console.WriteLine(favoriteNumberResponse + ", that is my 17th favorite number. HAHA.");
+
+
+            Console.WriteLine("How many pets do you have?");
+            string numberOfPets = Console.ReadLine();
+            int petNumber = int.Parse(numberOfPets);
+
+            string[] petNames = new string[petNumber];
+
+            for (int i = 0; i < petNumber; i++)
+            {
+                Console.WriteLine("What is the name of your pet?");
+                string petName = Console.ReadLine();
+                petNames[i] = petName;
+
+            }
+
+
+        }
+    }
+}
